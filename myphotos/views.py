1f5db115c821f0d05af810af3d2725cba0b1bcf5
NahianBinAlam <36653955+NahianBinAlam@users.noreply.github.com>
2021-04-05 04:17:55 +0600

    render data on template

--- myphotos/views.py
+++ myphotos/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Photo
 
 # Create your views here.
 def index(request):
-    return render(request, 'myphotos/index.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories': categories, 'photos': photos}
+    return render(request, 'myphotos/index.html', context)
 
 def viewphoto(request, pk):
-    return render(request, 'myphotos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'myphotos/photo.html',{'photo':photo})
     
 def addphoto(request):
     return render(request, 'myphotos/addphoto.html')
